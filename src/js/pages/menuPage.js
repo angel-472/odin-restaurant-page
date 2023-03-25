2abeb097fc7b12461816577c248169da29e92687
angel <108492882+angel-472@users.noreply.github.com>
2023-03-25 16:53:29 -0400

    Replace food icons with images

--- src/js/pages/menuPage.js
+++ src/js/pages/menuPage.js
@@ -6,32 +6,31 @@ const catCafeMenu = {
       name: "Tuna Sandwich",
       description: "Tuna, mayo, and lettuce on whole wheat bread",
       price: 7.99,
-      icon: "🥪",
       image: "/img/food/tuna-sandwich.png"
     },
     {
       name: "Grilled Cheese",
       description: "Melted cheddar cheese on sourdough bread",
       price: 6.99,
-      icon: "🧀"
+      image: "/img/food/grilled-cheese.jpg"
     },
     {
       name: "Fish Plate",
       description: "Grilled salmon served with steamed veggies and a side of rice",
       price: 12.99,
-      icon: "🐟"
+      image: "/img/food/fish-plate.jpg"
     },
     {
       name: "Croissant",
       description: "Freshly baked buttery croissant",
       price: 3.99,
-      icon: "🥐"
+      image: "/img/food/croissant.jpg"
     },
     {
       name: "Bagel with Cream Cheese",
       description: "Toasted bagel with cream cheese",
       price: 5.99,
-      icon: "🥯"
+      image: "/img/food/bagel.jpg"
     }
   ],
   drinks: [
@@ -39,25 +38,25 @@ const catCafeMenu = {
       name: "Catpuccino",
       description: "Espresso with steamed milk, topped with foam art of a cat",
       price: 5.99,
-      icon: "☕️"
+      image: "/img/food/catpuccino.jpg"
     },
     {
       name: "Catnip Tea",
       description: "Herbal tea made with organic catnip",
       price: 3.99,
-      icon: "🌿"
+      image: "/img/food/catnip-tea.jpg"
     },
     {
       name: "Iced Mocha",
       description: "Iced coffee with chocolate syrup and whipped cream",
       price: 4.99,
-      icon: "🥤"
+      image: "/img/food/iced-mocha.jpg"
     },
     {
       name: "Matcha Latte",
       description: "Green tea latte made with organic matcha",
       price: 6.99,
-      icon: "🍵"
+      image: "/img/food/matcha-latte.jpg"
     }
   ],
   treats: [
@@ -65,25 +64,25 @@ const catCafeMenu = {
       name: "Cat Cookies",
       description: "Freshly baked cookies in the shape of cats",
       price: 2.99,
-      icon: "🍪"
+      image: "/img/food/cat-cookies.jpg"
     },
     {
       name: "Cat Treats",
       description: "Organic cat treats made with all-natural ingredients",
       price: 3.99,
-      icon: "🐾"
+      image: "/img/food/cat-treats.jpg"
     },
     {
       name: "Cat Cake",
       description: "Vanilla cake with cream cheese frosting and decorated with cat faces",
       price: 6.99,
-      icon: "🎂"
+      image: "/img/food/cat-cake.jpg"
     },
     {
       name: "Fruit Salad",
       description: "Fresh fruit salad with honey dressing",
       price: 5.99,
-      icon: "🍓"
+      image: "/img/food/fruit-salad.jpg"
     }
   ]
 };
@@ -97,9 +96,7 @@ export class MenuPage extends Page {
     div.classList.add('food-card');
     let imgUrl = foodObject.image;
     div.innerHTML = `
-    <div class="food-icon">
-      <p>${foodObject.icon}</p>
-    </div>
+    <img src="${imgUrl || ''}" class="food-image">
     <div class="food-card-info">
       <p class="food-name">${foodObject.name}</p>
       <p class="food-description">${foodObject.description}</p>
